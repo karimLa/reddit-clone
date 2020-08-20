ef19282e6563fc8e440afa9d84f10c7b0c7f3ccb
karimLa <kira00luffy@gmail.com>
2020-08-20 19:05:35 +0100

    MikroORM TypeGraphQL CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
+import 'reflect-metadata';
+import express from 'express';
 import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 import mikroOrmConfig from './mikro-orm.config';
-import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 async function main() {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  const server = app.listen(4000, () => {
+    console.log(`> Server started on localhost:4000`);
+  });
+
+  server.on('error', (err) => {
+    console.log(err);
+    process.exit(1);
+  });
 }
 
 main().catch(console.error);
